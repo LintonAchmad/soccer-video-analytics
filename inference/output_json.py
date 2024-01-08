a4091d60e8c18c6a5ebc8c9963a8bcd04d2d66e4
Linton Achmad <linton.achmad@gmail.com>
2024-01-09 01:02:07 +0200

    get closest player bbox

--- inference/output_json.py
+++ inference/output_json.py
@@ -8,36 +8,19 @@ from soccer import Match
 class OutputJson:
     def write_detections(self, file_name, frame_id, detections, match: Match):
         sv_detections = Converter.Detections_to_Supervision(detections)
-        closest_player = []
+        closest_player = self.get_closest_player_bbox(match)
 
-        if match.closest_player is not None:
-            print(match.closest_player.detection.points)
-            xmin = match.closest_player.detection.points[0][0]
-            ymin = match.closest_player.detection.points[0][1]
-            xmax = match.closest_player.detection.points[1][0]
-            ymax = match.closest_player.detection.points[1][1]
-            print(xmin)
-            print(ymin)
-            print(xmax)
-            print(ymax)
-            closest_player = [
-                float(xmin),
-                float(ymin),
-                float(xmax),
-                float(ymax)
-            ]
+        initial_data = {
+            "frame_id": frame_id,
+            "ball_xyxy": json.dumps(sv_detections[sv_detections.class_id == 0].xyxy.tolist()),
+            "player_xyxy": json.dumps(sv_detections[sv_detections.class_id == 1].xyxy.tolist()),
+            "possession_counter": json.dumps(match.possession_counter),
+            "possession_team": json.dumps(match.team_possession.name),
+            "possession_team_counter": json.dumps(match.team_possession.possession),
+            "closest_player": json.dumps(closest_player)
+        }
 
-            initial_data = {
-                "frame_id": frame_id,
-                "ball_xyxy": json.dumps(sv_detections[sv_detections.class_id == 0].xyxy.tolist()),
-                "player_xyxy": json.dumps(sv_detections[sv_detections.class_id == 1].xyxy.tolist()),
-                "possession_counter": json.dumps(match.possession_counter),
-                "possession_team": json.dumps(match.team_possession.name),
-                "possession_team_counter": json.dumps(match.team_possession.possession),
-                "closest_player": json.dumps(closest_player)
-            }
-
-            self.append_to_json(file_name, initial_data)
+        self.append_to_json(file_name, initial_data) 
 
     def generate_random_filename(self):
         random_name = str(uuid.uuid4())  # Generates a random UUID
@@ -59,3 +42,21 @@ class OutputJson:
             # Write back to file
             with open(file_name, 'w') as file:
                 json.dump(data, file, indent=4)
+
+    def get_closest_player_bbox(self, match: Match):
+        if match.closest_player is not None:
+            xmin = match.closest_player.detection.points[0][0]
+            ymin = match.closest_player.detection.points[0][1]
+            xmax = match.closest_player.detection.points[1][0]
+            ymax = match.closest_player.detection.points[1][1]
+
+            closest_player = [
+                float(xmin),
+                float(ymin),
+                float(xmax),
+                float(ymax)
+            ]
+
+            return closest_player
+        else:
+            return []
